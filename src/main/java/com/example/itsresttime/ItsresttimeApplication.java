f8cfd08c0c9f526e0263e019e9278fa8743fb985
Aditya Kumar <aditya@ctrlmap.com>
2021-03-18 19:56:23 +0530

    changing

--- src/main/java/com/example/itsresttime/ItsresttimeApplication.java
+++ src/main/java/com/example/itsresttime/ItsresttimeApplication.java
@@ -8,7 +8,7 @@ public class ItsresttimeApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ItsresttimeApplication.class, args);
-		System.out.println("working ...");
+		System.out.println("if u see this means it's working working ...");
 	}
 
 }
